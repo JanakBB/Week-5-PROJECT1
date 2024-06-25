f5a2ba4e6fdc1c516d027ab86cbba8568b92bb75
Janak Bahadur Bohara <janakcustomx@gmail.com>
2024-06-25 19:31:07 +0545

    added error handles and logger

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,25 @@
 import express from "express";
+import notFoundHandler from "./middleware/notFoundMiddleware.js";
+import errorHandlor from "./middleware/errorMiddleWare.js";
+import logger from "./middleware/logger.js";
+
 
 //router imports
 import userRouter from "./routes/user.router.js";
+
 
 //Initialize express app
 const app = express();
 
 //middlewares
 app.use(express.json());
+app.use(logger)
 
 //routes
 app.use("/api/v1/users", userRouter);
+
+//error handler's
+app.use(notFoundHandler);
+app.use(errorHandlor);
 
 export { app };
---
+++ backend/middleware/logger.js
@@ -0,0 +1,30 @@
+import fs from "fs";
+// const colors = require("colors");
+import colors from "colors";
+
+function logger (req, res, next) {
+    let requestMethod = {
+        GET: "green",
+        POST: "yellow",
+        PUT: "blue",
+        DELETE: "red"
+    };
+
+    let start = Date.now();
+
+    res.on("finish", () => {
+        let end = Date.now();
+        let today = new Date();
+        let log = `${today.toLocaleDateString()} - ${req.method} - ${req.originalUrl} - ${req.ip} - ${res.statusCode} - ${end - start}ms`;
+        console.log(log[requestMethod[req.method]]);
+        fs.appendFile("logFile.txt", `\n${log}`, (err) => {
+            if(err) {
+                console.log("Error");
+            }
+        })
+    })
+   
+    next();
+}
+
+export default logger;
